f338bc60eaa4523be096756b42de8bb226597d09
Romario Raffington <romarioraff@gmail.com>
2017-07-15 16:28:14 -0500

    feat: Updated the default selected option to match the corresponding shelf

--- src/components/ShelfContainer/Shelf/BookList/index.js
+++ src/components/ShelfContainer/Shelf/BookList/index.js
@@ -16,7 +16,7 @@ export default class BookList extends Component {
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
                 <div className="book-shelf-changer">
-                  <select>
+                  <select defaultValue={book.shelf}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
