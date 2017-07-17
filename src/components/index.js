7c7de97a307c1e552e5a3c22c70935499078bce4
Romario Raffington <romarioraff@gmail.com>
2017-07-16 20:12:27 -0500

    feat: Added react router to navigate to the search and index page

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 // External Dependencies
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 // Our Dependencies
 import ShelfContainer from './ShelfContainer';
+import Search from './Search';
 import * as BooksAPI from '../BooksAPI';
 
 export default class App extends Component {
@@ -19,7 +21,7 @@ export default class App extends Component {
   shelfChange = (book, shelf) => {
     BooksAPI.update(book, shelf).then(data => {
       this.setState(({ books }) => ({
-        books: books.filter(b => 
+        books: books.filter(b =>
           b.id === book.id ? b.shelf = shelf : b
         )
       }));
@@ -29,17 +31,25 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
+        <Route exact path='/' render={({ history }) => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <ShelfContainer
+              books={this.state.books}
+              onShelfChange={this.shelfChange} />
+            <div className="open-search">
+              <a onClick={() => history.push('/search')}>
+                Add a book
+              </a>
+            </div>
           </div>
-          <ShelfContainer 
-            books={this.state.books} 
-            onShelfChange={this.shelfChange}/>
-          <div className="open-search">
-            <a onClick={() => {} }>Add a book</a>
-          </div>
-        </div>
+        )} />
+        <Route path='/search' render={({ history }) => (
+          <Search/>
+        )}
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// External Dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
+
+// Our Dependencies
 import App from './components'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+)
