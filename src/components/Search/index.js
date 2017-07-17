957cb6faed1e0a95adc2c23320b87ac02c13b440
Romario Raffington <romarioraff@gmail.com>
2017-07-16 21:09:40 -0500

    feat: Added query state to component and made component controlled.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,16 +5,27 @@ export default class Search extends Component {
   static propTypes = {
     onBackClick: PropTypes.func.isRequired,
   }
-  
+
+  state = {
+    query: '',
+  }
+
   render() {
+    const { query } = this.state;
     const { onBackClick } = this.props;
+
     return (
       <div>
         <div className="search-books">
           <div className="search-books-bar">
             <a className="close-search" onClick={() => onBackClick() }>Close</a>
             <div className="search-books-input-wrapper">
-              <input type="text" placeholder="Search by title or author" />
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(e) => {}}
+              />
             </div>
           </div>
           <div className="search-books-results">
