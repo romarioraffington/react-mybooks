1e26a95d804bab71daf32fb4e82bcc4b5b65a33c
Romario Raffington <romarioraff@gmail.com>
2017-07-17 01:37:57 -0500

    fix: Made it so no image is displayed for a book if the api does not return an image.  This prevents errors from being thrown in the console when book.imageLinks.thumbnail is undefined

--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -15,7 +15,7 @@ export default class BookList extends Component {
           <li key={book.id}>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail || '' }")` }}></div>
                 <div className="book-shelf-changer">
                   <select defaultValue={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
                     <option value="none" disabled>Move to...</option>
