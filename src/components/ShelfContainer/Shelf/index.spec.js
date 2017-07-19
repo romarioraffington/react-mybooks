0759925e1fce120b005135680f1476eec3663471
Romario Raffington <romarioraff@gmail.com>
2017-07-18 23:23:35 -0500

    test: Added the first test

---
+++ src/components/ShelfContainer/Shelf/index.spec.js
@@ -0,0 +1,35 @@
+// External Dependencies
+import React from 'react';
+import faker from 'faker';
+import { shallow } from 'enzyme';
+
+// Our Dependencies
+import Shelf from './index';
+import { expect } from '../../../util/chai';
+import { generateBookList } from '../../../util/testData';
+
+describe('Shelf', () => {
+  let wrapper;
+  const title = faker.lorem.word(),
+    books = generateBookList(),
+    onShelfChange = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Shelf
+        title={title}
+        books={books}
+        onShelfChange={onShelfChange}
+      />
+    )
+  });
+
+  afterEach(() => {
+    onShelfChange.mockClear();
+  });
+
+  it('should set the correct shelf title', () => {
+    const titleWrapper = wrapper.find('.bookshelf-title').first();
+    expect(titleWrapper).to.have.text(title);
+  });
+});
