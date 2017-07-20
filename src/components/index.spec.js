870355578527b7fc1264b430d58e2e7bef47e9bd
Romario Raffington <romarioraff@gmail.com>
2017-07-20 11:00:02 -0500

    test: Created tests for the App component

---
+++ src/components/index.spec.js
@@ -0,0 +1,41 @@
+// External Dependencies
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from 'enzyme'
+
+// Our Dependencies
+import App from './index';
+import * as BooksAPI from '../BooksAPI';
+import { expect } from '../util/chai';
+import { generateBookList } from '../util/testData';
+
+// Mock API calls
+jest.mock('../BooksAPI');
+
+describe.only('ShelfContainer',  () => {
+  let wrapper;
+  const books = generateBookList();
+  
+  beforeEach(() => {
+    // Mock Books.getAll implementation
+    BooksAPI.getAll = jest.fn().mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        resolve(books);
+      });
+    });
+
+    wrapper = mount(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    BooksAPI.getAll.mockClear();
+  });
+
+  it('should render without any errors', () => {
+    expect(wrapper).to.be.present();
+  });
+});
