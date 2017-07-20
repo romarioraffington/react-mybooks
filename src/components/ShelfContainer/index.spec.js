c28f096d3ccc2a71f904f94eb849ccd18893c8f5
Romario Raffington <romarioraff@gmail.com>
2017-07-20 10:59:27 -0500

    test: Created tests for the ShelfContainer component

---
+++ src/components/ShelfContainer/index.spec.js
@@ -0,0 +1,32 @@
+// External Dependencies
+import React from 'react';
+import { shallow } from 'enzyme'
+
+// Our Dependencies
+import ShelfContainer from './index';
+import { expect } from '../../util/chai';
+import { generateBookList } from '../../util/testData';
+
+
+describe('ShelfContainer', () => {
+  let wrapper;
+  const books = generateBookList(),
+    onShelfChange = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <ShelfContainer
+        books={books}
+        onShelfChange={onShelfChange}
+      />
+    )
+  });
+
+  afterEach(() => {
+    onShelfChange.mockClear();
+  });
+
+  it('should render without any errors', () => {
+    expect(wrapper).to.be.present();
+  })
+})
