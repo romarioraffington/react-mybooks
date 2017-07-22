b719f2b48070b71776fd39ff9472ef32bc96cc54
Romario Raffington <romarioraff@gmail.com>
2017-07-22 10:43:58 -0500

    feat: Made it so message is shown if a shelf is empty

--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -14,27 +14,35 @@ export default class BookList extends Component {
       <ol className="books-grid">    
         { isFetching ? 
           <div className="is-loading-shelf">Loading</div> : (
-          books.map(book => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail || '' }")` }}></div>
-                  <div className="book-shelf-changer">
-                    <select defaultValue={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
+            books.length > 0 ? (   
+              books.map(book => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail || '' }")` }}></div>
+                      <div className="book-shelf-changer">
+                        <select defaultValue={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
+                          <option value="none" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
                   </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
-              </div>
-            </li>
-          )
-        ))}
+                </li>
+              ))
+            ) : (
+              <li>
+                <h2 className="shelf-empty-message"> 
+                  Add 📚 to this shelf 🤓 
+                </h2>
+              </li>
+            ) 
+        )}
       </ol>
     )
   }
