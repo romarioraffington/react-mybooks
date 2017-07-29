df657c17b70fb48b788af0d61d1a62a6e393b134
Romario Raffington <romarioraff@gmail.com>
2017-07-29 18:26:30 -0500

    test: Updated test to match the current BookList component. I.E BookList is now a separate component from BookList

--- src/components/BookList/index.spec.js
+++ src/components/BookList/index.spec.js
@@ -9,7 +9,8 @@ import { generateBookList, getRandomShelf } from '../../util/testData';
 
 describe('BookList', () => {
   let wrapper;
-  const onShelfChange = jest.fn();
+  const onShelfChange = jest.fn(),
+    onBookClick = jest.fn();
   
   beforeEach(() => {
     wrapper = shallow(
@@ -17,12 +18,14 @@ describe('BookList', () => {
         books={[]}
         isFetching={false}
         onShelfChange={onShelfChange}
+        onBookClick={onBookClick}
       />
     )
   });
 
   afterEach(() => {
     onShelfChange.mockClear();
+    onBookClick.mockClear();
   });
 
   it('should not render any books', () => {
@@ -33,64 +36,19 @@ describe('BookList', () => {
     expect(wrapper).to.have.exactly(1).descendants('.shelf-empty-message');
   });
 
-  describe('pass in an array of books to books props', () => {
+  describe('when an array of books is passed as props', () => {
     const books = generateBookList();
     beforeEach(() => {
-      wrapper = shallow(
-        <BookList 
-          books={books}
-          isFetching={false}
-          onShelfChange={onShelfChange}
-      />
-      )
+      wrapper.setProps({
+          books,
+          isFetching: false,
+          onShelfChange,
+          onBookClick,
+      });
     });
 
     it(`should display ${books.length} books`, () => {
       expect(wrapper).to.have.exactly(books.length).descendants('li');
     });
-
-    it(`should select '${books[0].shelf}' as the shelf for the first book`, () => {
-      const select = wrapper.find('select').first();
-      expect(select).to.have.value(books[0].shelf);
-    });
-
-    it('should render the correct thumbnail url for the first book', () => {
-      const thumbnail = wrapper.find('.book-cover').first();
-      expect(thumbnail).to.have.text(books[0].thumbnail);
-    });
-
-    it('should render the correct title for the first book', () => {
-      const title = wrapper.find('.book-title').first();
-      expect(title).to.have.text(books[0].title);
-    });
-
-    it('should render the correct thumbnail author for the first book', () => {
-      const author = wrapper.find('.book-authors').first();
-      expect(author).to.have.text(books[0].author);
-    });
-    
-    describe('when user selects a different shelf option', () => {
-      const value = getRandomShelf();
-
-      beforeEach(() => {
-        const select = wrapper.find('select').first();
-        select.simulate('change', {
-          target: { value }
-        })
-      });
-      
-      it('should call `onShelfChange` function once', () => {
-        expect(onShelfChange.mock.calls.length).to.equal(1);
-      })
-
-      it('should pass the selected book object as the first param', () => {
-        expect(onShelfChange.mock.calls[0][0]).to.deep.equal(books[0]);
-      });
-
-      it('should pass the new shelf as a the second param', () => {
-        expect(onShelfChange.mock.calls[0][1]).to.equal(value);
-      })
-    });
-  });
-    
+  }); 
 });
