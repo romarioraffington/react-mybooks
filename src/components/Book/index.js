06ca61ebdf74041544eedbad6f2b8aa246e71280
Romario Raffington <romarioraff@gmail.com>
2017-07-30 02:24:37 -0500

    fix: Made it so an error is not thrown whenever a book does not have a thumbnail image url

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 
 const Book = ({ book, onShelfChange, onBookClick }) => {
 
-  const { thumbnail } = book.imageLinks;
-
-  // Replace 'http' image links with 'https'
+  // Replace 'http' thumbnail links with 'https'
   // to prevent mix content warnings in console
-  const imageUrl = thumbnail ? 
-    thumbnail.replace("http", "https") :
-    'https://books.google.com/googlebooks/images/no_cover_thumb.gif'
+  const thumbnail = book.imageLinks ?
+    book.imageLinks.thumbnail.replace("http://", "https://") :
+   'https://books.google.com/googlebooks/images/no_cover_thumb.gif';
 
   return (
     <div className="book">
@@ -17,7 +15,7 @@ const Book = ({ book, onShelfChange, onBookClick }) => {
         <div className="book-cover"
           style={{
             width: 128, height: 193,
-            backgroundImage: `url("${imageUrl}")`
+            backgroundImage: `url("${thumbnail}")`
           }}
           onClick={() => onBookClick && onBookClick(book) }>
         </div>
