476e806a957794afc85f1085a087a04c391836fb
Romario Raffington <romarioraff@gmail.com>
2017-07-30 00:51:52 -0500

    fix: Replace 'http' image links with 'https' for imageLinks to prevent mix content warnings in console

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,41 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book, onShelfChange, onBookClick }) => (
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover"
-        style={{
-          width: 128, height: 193,
-          backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")`
-        }}
-        onClick={() => onBookClick && onBookClick(book) }>
+const Book = ({ book, onShelfChange, onBookClick }) => {
+
+  const { thumbnail } = book.imageLinks;
+
+  // Replace 'http' image links with 'https'
+  // to prevent mix content warnings in console
+  const imageUrl = thumbnail ? 
+    thumbnail.replace("http", "https") :
+    'https://books.google.com/googlebooks/images/no_cover_thumb.gif'
+
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover"
+          style={{
+            width: 128, height: 193,
+            backgroundImage: `url("${imageUrl}")`
+          }}
+          onClick={() => onBookClick && onBookClick(book) }>
+        </div>
+        <div className="book-shelf-changer">
+          <select value={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
       </div>
-      <div className="book-shelf-changer">
-        <select value={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
-          <option value="none" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
+      {/* 
+        onBookClick will ONLY be defined when Book 
+        component is being rendered from the Search
+        or Home Screen. Therefore the code below 
+        will prevent the title and author from 
+        being displayed on other screens e.g Book Detail
+      */}
+      { onBookClick && (
+        <div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
+        </div>
+      )}
     </div>
-    {/* 
-      onBookClick will ONLY be defined when Book 
-      component is being rendered from the Search
-      or Home Screen. Therefore the code below 
-      will prevent the title and author from 
-      being displayed on other screens e.g Book Detail
-    */}
-    { onBookClick && (
-      <div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
-      </div>
-    )}
-  </div>
-)
+  )
+}
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
